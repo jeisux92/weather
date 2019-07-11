b963da26f27db640ba73e7189a8ac753fa342dc8
Gabriel Martinez <gabrielmartinez@lexicacorp.com>
2019-07-10 20:42:40 -0500

    fix some styles

--- src/components/SmallWeather/SmallWeather.js
+++ src/components/SmallWeather/SmallWeather.js
@@ -5,10 +5,12 @@ const SmallWeather = props => (
   <div className={classes.SmallWeather}>
     <h6>{props.day.substring(0, 3)}.</h6>
     <img src={props.weather.condition.icon} />
-    <h6>
-      <strong>{props.weather.maxtemp_c} °C</strong>
-    </h6>
-    <h6>{props.weather.mintemp_c} °C</h6>
+    <div className={classes.Temperature}>
+      <h6>
+        <strong>{props.weather.maxtemp_c} °C</strong>
+      </h6>
+      <h6>, {props.weather.mintemp_c} °C</h6>
+    </div>
   </div>
 );
 
